76e8249efb5e10ebfe4b035d2bce5d239abe5aa5
Dylan Johnson <dylanatasmsa@gmail.com>
2016-05-27 21:46:12 -0600

    update

---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://www.melpa.org/packages/"))
+
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+
+(global-auto-complete-mode t)
+
+(load-theme 'seti t)		
+
+(add-hook 'go-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode t)
+	    (setq tab-width 2)))
+	    
+
+(setq-default indent-tabs-mode nil)
+
